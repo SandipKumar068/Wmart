9dac14abc61fdee6b9557acbf53c8eccb914a68e
SandipKumar068 <dev.sandipkumar@gmail.com>
2018-02-13 01:23:20 +0530

    Add project files.

---
+++ WMart/Models/GuestResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WMart.Models
+{
+    public class GuestResponse
+    {
+        
+        public int CId { get; set; }
+       
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "First name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string CEmail { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        public string CPhone { get; set; }
+
+
+        public string Status { get; set; }
+    }
+}
